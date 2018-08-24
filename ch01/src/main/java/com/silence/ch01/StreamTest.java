d190fb7fff993ac2ed46aff2cbe46a85d0aa6cf5
silence <imsilence@outlook.com>
2018-08-24 08:10:49 +0800

    add ch01
    Signed-off-by: silence <imsilence@outlook.com>

---
+++ ch01/src/main/java/com/silence/ch01/StreamTest.java
@@ -0,0 +1,25 @@
+package com.silence.ch01;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class StreamTest {
+    public static void main( String[] args ) throws IOException {
+        String content = new String(Files.readAllBytes(Paths.get(StreamTest.class.getResource("words.txt").getPath())), StandardCharsets.UTF_8);
+        List<String> words = Arrays.asList(content.split(" "));
+        long count = words.stream().filter(word -> word.length() >= 5).count();
+        System.out.println(count);
+        
+
+        long count2 = words.parallelStream().filter(word -> word.length() >= 5).count();
+        System.out.println(count2);
+    }
+}
---
+++ corejava02.mm
@@ -0,0 +1,6 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1535027498616" ID="ID_1421677718" MODIFIED="1535027541617" TEXT="corejava02">
+<node CREATED="1535027553766" ID="ID_315921790" MODIFIED="1535027572867" POSITION="right" TEXT="01.Java SE 8&#x7684;&#x6d41;"/>
+</node>
+</map>
